d619ce10f705d04fc7255f1c27dd87ca4c8f50eb
Victor <victormanuelrealpepopayan@gmail.com>
2022-12-22 11:33:26 -0500

    falta img

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const path = require('path'); 
 const http = require('http');
 const PORT = process.env.PORT || 3000;
 const socket = require('socket.io');
@@ -7,16 +7,16 @@ const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
-server.listen(PORT,() => console.log("el servidor esta corriendo"));
+server.listen(PORT, () => console.log("El servidor esta corriendo"));
 
-const conections = [null,null];
+const connections = [null, null];
 
-io.on('connection', socket =>{
-    let playerIndex = 1;
-    for (const i in conections){
-        if (conections[i] === null){
+io.on('connection', socket => {
+    let playerIndex = -1;
+    for (const i in connections) {
+        if (connections[i] === null) {
             playerIndex = i;
             break;
         }
@@ -26,36 +26,34 @@ io.on('connection', socket =>{
 
     console.log(`Jugador ${playerIndex} se ha conectado`);
 
-    if(playerIndex === 1 ) return;
-    conections[playerIndex] = false;
+    if (playerIndex === -1) return;
+    connections[playerIndex] = false;
 
     socket.broadcast.emit('player-connection', playerIndex);
 
     socket.on('disconnect', () => {
-        console.log(`jugador0 ${playerIndex} se ha desconectado`);
-        conections[playerIndex] = null;
+        console.log(`Jugador ${playerIndex} se ha desconectado`);
+        connections[playerIndex] = null;
         socket.broadcast.emit('player-connection', playerIndex);
-
     });
 
     socket.on('player-ready', () => {
         socket.broadcast.emit('enemy-ready', playerIndex);
-        conections[playerIndex] = true;
-
+        connections[playerIndex] = true;
     });
 
-    socket.on('check-players', ()=>{
+    socket.on('check-players', () => {
         const players = [];
-        for (const i in conections){
-            conections[i] === null ?
-                players.push({connected: false, ready: false}) :
-                players.push({connected: true, ready: conections[i]});
+        for (const i in connections) {
+            connections[i] === null ? 
+                players.push({connected: false, ready: false}) : 
+                players.push({connected: true, ready: connections[i]});
         }
         socket.emit('check-players', players);
     });
 
-    socket.on('fire', id =>{
-        console.log(`disparo de ${playerIndex}`, id);
+    socket.on('fire', id => {
+        console.log(`Disparo de ${playerIndex}`, id);
         socket.broadcast.emit('fire', id);
     });
 
@@ -64,12 +62,10 @@ io.on('connection', socket =>{
         socket.broadcast.emit('fire-reply', square);
     });
 
-    setTimeout(()=>{
-        conections[playerIndex] = null;
+    setTimeout(() => {
+        connections[playerIndex] = null;
         socket.emit('timeout');
         socket.disconnect();
     }, 600000);
-
-
 
 });
